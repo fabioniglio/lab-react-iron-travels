c4cfb504a6d662a65bc003202a0757ff7badf543
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-07 14:44:19 +0200

    travel list button delete

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,25 +1,60 @@
+import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
-import classes from "./TravelList.module.css";
+import classes from "../styles/travelList.module.css";
 
 function TravelList() {
+  const [travelPlans, setTravelPlans] = useState(travelPlansData);
+
+  const handleDelete = (travelId) => {
+    console.log(travelId);
+    setTravelPlans(
+      travelPlans.filter(
+        (currentDestination) => currentDestination.Id !== travelId
+      )
+    );
+    console.log(travelPlans);
+  };
+
   return (
-    <ul>
-      {travelPlansData.map((currentDestination) => (
-        <li key={currentDestination.id} className={classes}>
-          <img src={currentDestination.image} className={classes.img} alt="" />
-          <div>
-            <h3>{currentDestination.destination}</h3>
-            <p>{currentDestination.description}</p>
-            <p>
-              <strong>Price: </strong>
-              {currentDestination.totalCost} Euro
-            </p>
-            <p>Premium</p>
-            <p>All-inclusive</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {travelPlans.map((currentDestination) => (
+          <li key={currentDestination.id} className={classes.ctn}>
+            <img
+              src={currentDestination.image}
+              className={classes.img}
+              alt=""
+            />
+            <div>
+              <h3>{currentDestination.destination}</h3>
+              <p>{currentDestination.description}</p>
+              <p>
+                <strong>Price: </strong>
+                {currentDestination.totalCost} Euro
+              </p>
+              <div className={classes.conditions}>
+                {currentDestination.totalCost >= 1500 && (
+                  <p className={classes.blueLabel}>Premium</p>
+                )}
+                {currentDestination.allInclusive && (
+                  <p className={classes.blueLabel}>All-inclusive</p>
+                )}
+
+                {currentDestination.totalCost <= 350 && (
+                  <p className={classes.greenLabel}>Great Deal</p>
+                )}
+              </div>
+              <button
+                type="button"
+                onClick={() => handleDelete(currentDestination.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
